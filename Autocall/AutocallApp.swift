1af341e43bf3228330f53c221dae8fce5f700460
briahnloo <brian.liu2021@gmail.com>
2024-06-23 19:15:57 -0700

    Initial Commit

---
+++ Autocall/AutocallApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutocallApp.swift
+//  Autocall
+//
+//  Created by Brian Liu on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AutocallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
